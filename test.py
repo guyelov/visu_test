f5e70c6f12be75d116f686c0ac29a2c437670312
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-14 22:55:50 +0300

    test

--- test.py
+++ test.py
@@ -2,4 +2,19 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 dataframe = pd.read_csv('player_stats_dataset.csv')
+# Set page config
+st.set_page_config(page_title='Soccer Players Stats', page_icon=':soccer:', initial_sidebar_state='expanded')
+
+games_list = list(dataframe['player'])
+
+# Drop-down menu "Select Football Game"
+st.sidebar.markdown('## Select Player ')
+menu_game = st.sidebar.selectbox('Select Game', games_list, index=14)
+st.sidebar.markdown("""Here you can select one of 15 football games from the UEFA Euro 2020 knockout stage: """)
+st.sidebar.markdown(""" 
+                    * eight games in the round of 16
+                    * four quarter-finals
+                    * two semi-finals
+                    * one final
+                    """)
 st.dataframe(dataframe)
