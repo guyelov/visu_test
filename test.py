ca200bdb3c80283c412179f8317ecdd8961a316f
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-14 23:41:08 +0300

    test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ dataframe = pd.read_csv('player_stats_dataset.csv')
 st.set_page_config(page_title='Soccer Players Stats', page_icon=':soccer:', initial_sidebar_state='expanded')
 
 games_list = sorted(list(set(dataframe['player'])))
-
+players = pd.read_csv('players_details.csv')
 # Drop-down menu "Select Football Game"
 st.sidebar.markdown('## Select Player ')
 menu_game = st.sidebar.selectbox('Select a player at your choice', games_list, index=14)
@@ -17,6 +17,7 @@ st.sidebar.markdown("""
                     * two semi-finals
                     * one final
                     """)
-st.write(f'my name is {menu_game} i born in England :england:  ')
+selected_player =players. loc[players['Name'] == menu_game]
+st.write(f'my name is {menu_game} i born in {selected_player["Nationality"][0]} :england:  ')
 
 st.dataframe(dataframe.loc[dataframe['player'] == menu_game])
