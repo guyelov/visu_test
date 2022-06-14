040cae6cb67c81fe1077bddbd4eb4a153d52dcd5
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-15 00:34:30 +0300

    test

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import flag
 
 dataframe = pd.read_csv('player_stats_dataset.csv')
 # Set page config
@@ -14,4 +13,4 @@ st.sidebar.markdown('## Select Player ')
 menu_game = st.sidebar.selectbox('Select a player at your choice', games_list, index=14)
 
 selected_player =players. loc[players['Name'] == menu_game]
-st.write(f'my name is {menu_game} i born in {selected_player["Nationality"].iloc[0]} {flag.string(selected_player["Nationality"].iloc[0])} in {selected_player["Birth_Date"].iloc[0]}  ')
+st.write(f'my name is {menu_game} i born in {selected_player["Nationality"].iloc[0]}  in {selected_player["Birth_Date"].iloc[0]}  ')
