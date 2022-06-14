d6dd63310968b1eb94861f1f9e04f998e7426bc1
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-14 23:52:19 +0300

    test

--- test.py
+++ test.py
@@ -11,12 +11,7 @@ players = pd.read_csv('players_details.csv')
 st.sidebar.markdown('## Select Player ')
 menu_game = st.sidebar.selectbox('Select a player at your choice', games_list, index=14)
 st.sidebar.markdown("""Here you can select one of 15 football games from the UEFA Euro 2020 knockout stage: """)
-st.sidebar.markdown(""" 
-                    * eight games in the round of 16
-                    * four quarter-finals
-                    * two semi-finals
-                    * one final
-                    """)
+
 selected_player =players. loc[players['Name'] == menu_game]
 st.write(f'my name is {menu_game} i born in {selected_player["Nationality"].iloc[0]} in {selected_player["Birth_Date"].iloc[0]}  ')
 
