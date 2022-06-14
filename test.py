1340e4d334f858693e4582f320e1e18d03fb7795
guyelov <87119884+guyelov@users.noreply.github.com>
2022-06-15 00:34:55 +0300

    test

--- test.py
+++ test.py
@@ -13,4 +13,4 @@ st.sidebar.markdown('## Select Player ')
 menu_game = st.sidebar.selectbox('Select a player at your choice', games_list, index=14)
 
 selected_player =players. loc[players['Name'] == menu_game]
-st.write(f'my name is {menu_game} i born in {selected_player["Nationality"].iloc[0]}  in {selected_player["Birth_Date"].iloc[0]}  ')
+st.write(f'my name is {menu_game} i born in {selected_player["Nationality"].iloc[0]} in {selected_player["Birth_Date"].iloc[0]}  ')
